8a4f4d84e7fa89b986d804d7932d4ed442ab38c0
Kim Young Jo <yoyounn88@gmail.com>
2021-04-09 14:42:37 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1,
-        text: 'Good Morning',
-        day: 'Monday',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Good Afternoon',
-        day: 'Tuesday',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Good Night',
-        day: 'Wendsday',
-        reminder: false,
-      }
-    ]
-  )
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    console.log(data)
+    return data
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    console.log(data)
+    return data
+  }
 
   // filter를 이용하여 받아온 id props와 다른 것들로 새로운 task 배열을 return하여 제거의 효과를 만듦.
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // map을 이용하여 받아온 id props의 객체의 reminder(boolean) 값을 바꿔줌
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
   }
 
   // 새로 추가한 객체를 props로 받아와 random을 이용하여 임의의 id를 부여한 후 기존 tasks(...tasks)에 추가함
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
 
-    setTasks([...tasks, newTask])
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+
+    // setTasks([...tasks, newTask])
   }
 
   //Add Task Component를 toggle 형식으로 펼쳤다 접었다를 할 수 있는 function
@@ -56,6 +94,7 @@ const App = () => {
       <Header onClick={showAddTaskBtn} btnText={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks'}
+      <Footer />
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,8 +6,8 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
         <>
             {
                 // map()에 대한 활용
-                tasks.map((task) => (
-                    <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+                tasks.map((task, index) => (
+                    <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
                 ))
             }
         </>
